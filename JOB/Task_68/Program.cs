f45d06614c02123063b33a43cc10e09804e99643
Arkashka <sevenkapsul@mail.ru>
2022-08-20 17:11:07 +0300

    Task_68_Completed

--- JOB/Task_68/Program.cs
+++ JOB/Task_68/Program.cs
@@ -4,6 +4,23 @@
 с помощью рекурсии. 
 Даны два неотрицательных числа m и n.
 
-m = 2, n = 3 -> A(m,n) = 29
+m = 2, n = 3 -> A(m,n) = 9
 */
 
+// string[] input = Console.ReadLine().Split(",");
+// int m = int.Parse(input[0]);
+// int n = int.Parse(input[1]);
+Console.WriteLine("Введите значение -m- :");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение -n- :");
+int n = int.Parse(Console.ReadLine());
+
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return FunctionAkkerman(m - 1, 1);
+    else return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
+}
+
+Console.WriteLine("Функция Аккермана:" + $"\nA({m},{n}) = {FunctionAkkerman(m,n)}");
