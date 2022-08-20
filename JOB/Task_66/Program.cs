fa5be1819c77604331cdd9fbb8a47f70154f3eca
Arkashka <sevenkapsul@mail.ru>
2022-08-20 14:26:12 +0300

    Task_66_Completed

--- JOB/Task_66/Program.cs
+++ JOB/Task_66/Program.cs
@@ -8,3 +8,43 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+Console.Write("Введите число -m- : ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число -n- : ");
+int n = int.Parse(Console.ReadLine());
+
+int SummaNaturalNumbersNM(int start, int stop)
+{
+    int summa = 0;
+
+    while (true)
+    {
+        if (start <= stop)
+        {
+            summa += start;
+            start++;
+        }
+        else
+        {
+            return summa;
+        }
+    }
+}
+
+int SummaNaturalNumbersNM2(int start, int stop, int summa = 0)
+{
+    if (start > stop) return summa;
+
+    else
+    {
+        summa += start;
+        return SummaNaturalNumbersNM2(++start, stop, summa);
+    }
+}
+
+
+
+Console.WriteLine("Обычный метод:");
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна: {SummaNaturalNumbersNM(m, n)}");
+Console.WriteLine("Рекурсивный метод:");
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна: {SummaNaturalNumbersNM2(m, n)}");
